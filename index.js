b686baa48de9c86998a0f7b4366c238795b8943a
Rochael-Reyes <reyes2030327@mls.ceu.edu.ph>
2023-04-14 21:15:52 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const number = parseInt(prompt('Enter a positive integer: '));
+let num1 = 0, num2 = 1, nTerm;
+
+console.log('Fibonacci Series:');
+console.log(num1); 
+console.log(num2); 
+
+nextTerm = num1 + num2;
+
+while (nTerm <= number) {
+
+    console.log(nTerm);
+
+    num1 = num2;
+    num2 = ntTerm;
+    nTerm = num1 + num2;
+}
